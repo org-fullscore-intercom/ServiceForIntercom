02af97970566c88d227d08879345e4efeb6e0c4c
toreme <1306463067@qq.com>
2023-03-31 11:03:52 +0800

    springboot configuration

---
+++ src/main/java/com/amazing/intercom/web/HelloController.java
@@ -0,0 +1,12 @@
+package com.amazing.intercom.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello Sprint Boot";
+    }
+}
---
+++ src/test/java/com/amazing/intercom/IntercomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazing.intercom;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntercomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
