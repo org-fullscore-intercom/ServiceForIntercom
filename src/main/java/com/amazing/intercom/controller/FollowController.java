f8c519bca77a09e65b60484f1ddd9f1530d22103
toreme <1306463067@qq.com>
2023-04-12 16:00:23 +0800

    modify the follow API about the follows and fans's result

--- src/main/java/com/amazing/intercom/controller/FollowController.java
+++ src/main/java/com/amazing/intercom/controller/FollowController.java
@@ -2,18 +2,25 @@ package com.amazing.intercom.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.amazing.intercom.pojo.Follow;
+import com.amazing.intercom.pojo.RoomRecord;
+import com.amazing.intercom.pojo.User;
 import com.amazing.intercom.service.FollowService;
+import com.amazing.intercom.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class FollowController {
     @Autowired
     private FollowService followService;
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/addFollow")
     @ResponseBody
     public String add(int u_id,int f_u_id){
@@ -34,12 +41,20 @@ public class FollowController {
     @ResponseBody
     public String follows(int u_id){
         List<Follow> follows = followService.getFollows(u_id);
-        return JSON.toJSONString(follows);
+        ArrayList<User> users=new ArrayList<>();
+        for (Follow f : follows){
+            users.add(userService.getUser(f.getF_u_id()));
+        }
+        return JSON.toJSONString(users);
     }
     @GetMapping("/fans")
     @ResponseBody
     public String fans(int f_u_id){
         List<Follow> fans = followService.getFans(f_u_id);
-        return JSON.toJSONString(fans);
+        ArrayList<User> users=new ArrayList<>();
+        for (Follow f : fans){
+            users.add(userService.getUser(f.getU_id()));
+        }
+        return JSON.toJSONString(users);
     }
 }
