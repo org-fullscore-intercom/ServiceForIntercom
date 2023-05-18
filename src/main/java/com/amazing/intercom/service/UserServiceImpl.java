d7821451515f64acdfa6a54b4a59f6e83e205e6a
toreme <1306463067@qq.com>
2023-05-18 13:34:10 +0800

    用户修改用户信息时对account属性的避免重复控制

--- src/main/java/com/amazing/intercom/service/UserServiceImpl.java
+++ src/main/java/com/amazing/intercom/service/UserServiceImpl.java
@@ -36,6 +36,15 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public boolean modifyUser(User user) {
+        User realU = userDAO.get(user.getId());//获取旧的user信息，与将要修改的进行比较
+        if (!realU.getAccount().equals(user.getAccount())) { //如果要修改account属性，因为要增加 account 不能重复的限制，这里进行处理
+            if (userDAO.getByAccount(user.getAccount())==0){ //如果当前数据库中不存在该account记录，可以修改account
+                return userDAO.update(user)==1;
+            }else{ //已存在相同的account记录，请更换account
+                return false;
+            }
+        }
+        //如果不修改account，直接更新
         return userDAO.update(user)==1;
     }
 }
